cb92ad76bf12dd87e31f2535e9360bdb8bb96c17
ukorvl <ukorotovskiy@gmail.com>
2022-08-29 21:40:34 +0400

    add storybook theming #1

---
+++ .storybook/theme.js
@@ -0,0 +1,16 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  // colors
+  base: 'dark',
+  colorPrimary: '#222222',
+  colorSecondary: '#3C3C3C',
+  // brand
+  brandTitle: '=nil; Foundation',
+  brandUrl: 'https://nil.foundation',
+  brandTarget: '_blank',
+  textColor: '#F5F2F1',
+  appBorderRadius: 4,
+  // bar
+  barSelectedColor: '#CAC0C9'
+});
