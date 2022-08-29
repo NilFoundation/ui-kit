99a796aa211847a0fb705240f81e1ad0e18c8e11
ukorvl <ukorotovskiy@gmail.com>
2022-08-29 13:16:25 +0400

    change useKeyPress settings to object #1

--- src/hooks/useKeyPress.tsx
+++ src/hooks/useKeyPress.tsx
@@ -7,27 +7,37 @@ import { KeyboardEvent as ElementKeyboardEvent } from 'react';
 import { KeyboardEventKey } from '../enums';
 
 /**
+ * Hook settings.
+ */
+type UseKeyPressSettings = {
+    /**
+     * Prevent default browser events.
+     */
+    preventDefault?: boolean;
+};
+
+/**
  * Returns callback with key pressed checking.
  * If allowedKeys are empty, no callback execution will happen.
  *
  * @param callback - Callback.
  * @param allowedKeys - Array of allowed keys.
- * @param [preventDefault = true] - Prevent default browser events.
+ * @param {UseKeyPressSettings} settings - Prevent default browser events.
  * @returns - Key event listener.
  */
 export const useKeyPress = <T extends KeyboardEvent | ElementKeyboardEvent>(
     callback: () => void,
     allowedKeys: KeyboardEventKey[],
-    preventDefault = true,
+    { preventDefault }: UseKeyPressSettings = { preventDefault: true },
 ): [onKeyPress: (e: T) => void] => {
     const onKeyPress = (e: T) => {
+        if (allowedKeys.length === 0) {
+            return;
+        }
+
         if (preventDefault) {
             e.preventDefault();
             e.stopPropagation();
-        }
-
-        if (allowedKeys.length === 0) {
-            return;
         }
 
         const { key } = e;
