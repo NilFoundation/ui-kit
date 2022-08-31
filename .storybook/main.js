e8f09c8930b70f5230d222423222557e380cf925
ukorvl <ukorotovskiy@gmail.com>
2022-08-31 14:44:09 +0400

    use roboto font in stories #1

--- .storybook/main.js
+++ .storybook/main.js
@@ -19,5 +19,9 @@ module.exports = {
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.0/dist/css/bootstrap.min.css"
     />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+      rel="stylesheet"
+    />
   `)
 }
