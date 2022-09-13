22bb690be2d042e192b85fdd6100b1137890e205
ukorvl <ukorotovskiy@gmail.com>
2022-09-14 03:54:34 +0400

    add useHover settings and handle hover on child elements #1

--- src/hooks/useHover.tsx
+++ src/hooks/useHover.tsx
@@ -4,7 +4,7 @@
  */
 
 import { useEffect, useState, useRef, RefObject } from 'react';
-import { throttle } from '../helpers';
+import { debounce } from '../helpers';
 
 /**
  * useHover hook return type.
@@ -12,16 +12,64 @@ import { throttle } from '../helpers';
 export type UseHoverReturnType<T extends HTMLElement> = [RefObject<T>, boolean];
 
 /**
+ * Hook settings.
+ */
+export type UseHoverSettings = {
+    /**
+     * Delay between hover an element and callback call.
+     *
+     * @default 100
+     */
+    hoverInDelay?: number;
+    /**
+     * Delay between leaving element and callback call.
+     *
+     * @default 100
+     */
+    hoverOutDelay?: number;
+};
+
+/**
  * Detect whether the mouse is hovering an element.
  *
+ * @param {UseHoverSettings} settings - Settings.
  * @returns {UseHoverReturnType} ref and hover state.
  */
-export const useHover = <T extends HTMLElement>(): UseHoverReturnType<T> => {
+export const useHover = <T extends HTMLElement>({
+    hoverInDelay = 100,
+    hoverOutDelay = 100,
+}: UseHoverSettings = {}): UseHoverReturnType<T> => {
     const [isHover, setIsHover] = useState(false);
     const ref = useRef<T>(null);
 
-    const handleMouseOver = throttle(() => setIsHover(true));
-    const handleMouseOut = throttle(() => setIsHover(false));
+    const handleMouseEvent = (
+        { relatedTarget, target }: MouseEvent,
+        element: T,
+        callback: () => void,
+    ) => {
+        const isMovingToChildNode =
+            relatedTarget instanceof Node &&
+            !element.isSameNode(relatedTarget) &&
+            element.contains(relatedTarget);
+
+        const isMovingFromChildNode =
+            target instanceof Node && !element.isSameNode(target) && element.contains(target);
+
+        if (isMovingToChildNode || isMovingFromChildNode) {
+            return;
+        }
+
+        callback();
+    };
+
+    const handleMouseOver = debounce(() => {
+        setIsHover(true);
+    }, hoverInDelay);
+
+    const handleMouseOut = debounce(
+        (evt: MouseEvent, element: T) => handleMouseEvent(evt, element, () => setIsHover(false)),
+        hoverOutDelay,
+    );
 
     useEffect(() => {
         const el = ref.current;
@@ -31,12 +79,13 @@ export const useHover = <T extends HTMLElement>(): UseHoverReturnType<T> => {
         }
 
         el.addEventListener('mouseover', handleMouseOver);
-        el.addEventListener('mouseout', handleMouseOut);
+        el.addEventListener('mouseout', (e: MouseEvent) => handleMouseOut(e, el));
 
         return () => {
             el.removeEventListener('mouseover', handleMouseOver);
-            el.removeEventListener('mouseout', handleMouseOut);
+            el.removeEventListener('mouseout', (e: MouseEvent) => handleMouseOut(e, el));
         };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [ref.current]);
 
     return [ref, isHover];
