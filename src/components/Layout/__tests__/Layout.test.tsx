9608b73ca384e29907bdf87ca3202dbef7431a76
ukorvl <ukorotovskiy@gmail.com>
2022-09-15 00:42:59 +0400

    layout test added #1

---
+++ src/components/Layout/__tests__/Layout.test.tsx
@@ -0,0 +1,26 @@
+/**
+ * @file Unit test.
+ * @copyright Yury Korotovskikh 2022 <u.korotovskiy@nil.foundation>
+ */
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Layout } from '../Layout';
+
+/**
+ * Layout component unit testing.
+ */
+describe('layout', () => {
+    /**
+     * Component renders.
+     */
+    it('renders', () => {
+        const { getByRole } = render(
+            <Layout>
+                <div />
+            </Layout>,
+        );
+
+        expect(getByRole('main')).toBeInTheDocument();
+    });
+});
