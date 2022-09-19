d178ffecb6522a61082719d8b9998a12f7d39757
ukorvl <ukorotovskiy@gmail.com>
2022-09-20 03:24:27 +0400

    grid stories added #1

---
+++ src/components/Grid/stories/Grid.stories.tsx
@@ -0,0 +1,63 @@
+/**
+ * @file Stories.
+ * @copyright Yury Korotovskikh 2022 <u.korotovskiy@nil.foundation>
+ */
+
+import React from 'react';
+import { Args, Story } from '@storybook/react';
+import { Col, Row, Container } from '..';
+
+export default {
+    title: 'layout/Grid',
+    argTypes: {
+        children: {
+            table: {
+                disable: true,
+            },
+        },
+        className: {
+            table: {
+                disable: true,
+            },
+        },
+    },
+    args: {
+        fluid: false,
+        noGutters: false,
+    },
+};
+
+export const Grid: Story = ({ fluid, noGutters }: Args) => (
+    <Container fluid={fluid}>
+        <Row noGutters={noGutters}>
+            <Col
+                lg={4}
+                md={6}
+                sm={9}
+                xs={12}
+            >
+                <div style={{ backgroundColor: 'peachpuff' }}>lg={4} / md=6 / sm=9 / xs=12</div>
+            </Col>
+            <Col
+                lg={4}
+                md={3}
+                sm={12}
+                xs={6}
+            >
+                <div style={{ backgroundColor: 'ghostwhite' }}>lg={4} / md=3 / sm=12 / xs=6</div>
+            </Col>
+            <Col
+                lg={4}
+                md={2}
+                sm={4}
+                xs={8}
+                xs-offset={3}
+                sm-offset={0}
+            >
+                <div style={{ backgroundColor: 'skyblue' }}>
+                    lg={4} / md=2 / sm=4 / xs=8 / xs-offset=3
+                </div>
+            </Col>
+        </Row>
+    </Container>
+);
