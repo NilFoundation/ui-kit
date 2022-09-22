ace17751f0bc0386df3147689c6df04978079e47
ukorvl <ukorotovskiy@gmail.com>
2022-09-23 01:08:43 +0400

    forwardRef in Menu added #1

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -3,9 +3,20 @@
  * @copyright Yury Korotovskikh 2022 <u.korotovskiy@nil.foundation>
  */
 
-import React, { ReactElement, ReactNode, useRef, HTMLAttributes } from 'react';
+import React, {
+    ReactNode,
+    HTMLAttributes,
+    forwardRef,
+    ForwardRefExoticComponent,
+    RefAttributes,
+} from 'react';
 import clsx from 'clsx';
-import { useClickOutsideCallback, useEventListener, useKeyPress } from '../../hooks';
+import {
+    useClickOutsideCallback,
+    useCombinedRef,
+    useEventListener,
+    useKeyPress,
+} from '../../hooks';
 import { MenuHeader } from './MenuHeader';
 import { MenuItem } from './MenuItem';
 import { KeyboardEventKey } from '../../enums';
@@ -31,21 +42,24 @@ export type MenuProps = {
 } & HTMLAttributes<HTMLUListElement>;
 
 /**
+ * Compounded component type.
+ */
+type CompoundedMenuComponent = {
+    Header: typeof MenuHeader;
+    Item: typeof MenuItem;
+    Divider: typeof MenuDivider;
+} & ForwardRefExoticComponent<MenuProps & RefAttributes<HTMLUListElement>>;
+
+/**
  * Menu component.
  *
  * @param {MenuProps} props - Props.
  * @returns React component.
  */
-export const Menu = ({
-    children,
-    className,
-    onCloseMenu,
-    visible,
-    role = 'menu',
-    ...rest
-}: MenuProps): ReactElement => {
-    const ref = useRef<HTMLUListElement>(null);
-
+export const Menu = forwardRef<HTMLUListElement, MenuProps>(function Menu(
+    { children, className, onCloseMenu, visible, role = 'menu', ...rest }: MenuProps,
+    ref,
+) {
     const menuClassName = clsx('dropdown-menu', className && className, !visible && 'hidden');
 
     const onCloseHandler = (): void => {
@@ -54,7 +68,9 @@ export const Menu = ({
 
     const [onEscapePress] = useKeyPress(onCloseHandler, [KeyboardEventKey.escape]);
 
-    useClickOutsideCallback(ref, onCloseHandler, visible);
+    const { localRef, forwardedRef } = useCombinedRef(ref);
+
+    useClickOutsideCallback(localRef, onCloseHandler, visible);
     useEventListener({
         eventType: 'scroll',
         throttled: true,
@@ -66,13 +82,13 @@ export const Menu = ({
         <ul
             className={menuClassName}
             role={role}
-            ref={ref}
+            ref={forwardedRef}
             {...rest}
         >
             {children}
         </ul>
     );
-};
+}) as CompoundedMenuComponent;
 
 /**
  * Component extensions.
