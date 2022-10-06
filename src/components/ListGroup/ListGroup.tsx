110e84804770589aa1a7811ab28e0a03f561134f
ukorvl <ukorotovskiy@gmail.com>
2022-10-06 18:06:13 +0400

    forward ref ListGroup component #1

--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -3,7 +3,13 @@
  * @copyright Yury Korotovskikh 2022 <u.korotovskiy@nil.foundation>
  */
 
-import React, { ReactElement, ReactNode } from 'react';
+import React, {
+    forwardRef,
+    ForwardRefExoticComponent,
+    ReactElement,
+    ReactNode,
+    RefAttributes,
+} from 'react';
 import clsx from 'clsx';
 import { ListGroupItem } from './ListGroupItem';
 
@@ -21,24 +27,35 @@ export interface ListGroupProps {
     className?: string;
 }
 
+/*
+ * Compounded ListGroup component type.
+ */
+type CompoundedListGroupComponent = {
+    Item: typeof ListGroupItem;
+} & ForwardRefExoticComponent<ListGroupProps & RefAttributes<HTMLDivElement>>;
+
 /**
  * List group component.
  *
  * @param {ListGroupProps} props - Props.
  * @returns - React component.
  */
-export const ListGroup = ({ children, className }: ListGroupProps): ReactElement => {
+export const ListGroup = forwardRef<HTMLDivElement, ListGroupProps>(function ListGroup(
+    { children, className }: ListGroupProps,
+    ref,
+): ReactElement {
     const listClassName = clsx('list-group', className && className);
 
     return (
         <div
             className={listClassName}
             role="list"
+            ref={ref}
         >
             {children}
         </div>
     );
-};
+}) as CompoundedListGroupComponent;
 
 /**
  * Component extensions.
