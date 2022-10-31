316f7bc152833f7171b973e9679aad7c68ce07b3
ukorvl <ukorotovskiy@gmail.com>
2022-10-31 22:02:20 +0400

    target prop HTMLAttributeAnchorTarget type #1

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -3,7 +3,7 @@
  * @copyright Yury Korotovskikh 2022 <u.korotovskiy@nil.foundation>
  */
 
-import React, { ReactElement, ReactNode } from 'react';
+import React, { HTMLAttributeAnchorTarget, ReactElement, ReactNode } from 'react';
 import clsx from 'clsx';
 import { Variant } from '../../enums';
 import { CloseButton } from '../Button';
@@ -40,7 +40,7 @@ export type LabelProps = {
     /**
      * Specify target for link wrapper.
      */
-    target?: string;
+    target?: HTMLAttributeAnchorTarget;
     /**
      * Close callback.
      */
