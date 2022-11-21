7e9cad8ce1e5b8532a251b1d5c4d12efc97f9d7e
ukorvl <ukorotovskiy@gmail.com>
2022-11-22 00:10:50 +0400

    copy entire styles folder into build dir #10

--- rollup.config.js
+++ rollup.config.js
@@ -48,7 +48,7 @@ export default [
             copy({
                 targets: [
                     {
-                        src: 'src/styles/_variables.scss',
+                        src: 'src/styles',
                         dest: 'build',
                     },
                 ]
