bc4beb1e56b9728de2d84b5eaef62242b90b162f
ukorvl <ukorotovskiy@gmail.com>
2023-02-02 22:47:26 +0400

    add enum color #17

--- src/color.ts
+++ src/color.ts
@@ -3,22 +3,20 @@
  * @copyright Yury Korotovskikh <u.korotovskiy@nil.foundation>
  */
 
-import colors from './scss/colors.module.scss';
-
-// TODO - move to enum after having typescript@5
-// see https://github.com/microsoft/TypeScript/pull/50528
-
 /**
  * Theme color scheme.
  */
-export const color = {
-    primary: colors.primary,
-    danger: colors.danger,
-    success: colors.success,
-    baseColor: colors.baseColor,
-    info: colors.info,
-    baseDarker: colors.baseDarker,
-    baseLight: colors.baseLight,
-    secondary: colors.secondary,
-    secondaryDarker: colors.secondaryDarker,
-};
+export enum color {
+    primary = '#776475',
+    danger = '#dc3545',
+    success = '#198754',
+    info = '#2677f0',
+    baseColor = '#222222',
+    baseDarker = '#151515',
+    baseLight = '#292929',
+    baseLighter = '#3c3c3c',
+    secondary = '#f5f2f1',
+    secondaryDarker = '#d3d3d3',
+    secondaryDark = '#6c757d',
+    disabledColor = 'rgba(#d3d3d3, 0.7)',
+}
