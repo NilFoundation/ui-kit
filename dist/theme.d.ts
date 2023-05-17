68df4d45d6e9b01c748f97b85abb279f808c5156
KlonD90 <newbiecraft@gmail.com>
2023-05-17 21:12:15 +0700

    move to new ui-kit clean repo and add example of theme and ButtonExample

---
+++ dist/theme.d.ts
@@ -0,0 +1,1 @@
+export declare const theme: import("baseui").Theme;
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "baseui";
+
+// this code is example
+export const theme = createTheme(
+  {},
+  {
+    colors: {
+      primary: "#000",
+      accent: "#fff",
+      accent50: "#fff",
+    },
+  }
+);
