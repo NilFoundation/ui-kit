125abf583ab2290d5d58749f75d3a68362c9f198
Gor Grigoryan <gor.grigoryan@saltpepper.ru>
2023-06-26 12:25:44 +0300

    add input modifications

--- src/components/input/overrides.tsx
+++ src/components/input/overrides.tsx
@@ -2,13 +2,17 @@ import { InputOverrides } from "baseui/input";
 import { inputContainerModifiedStyles, inputDisabledStyles, inputModifiedStyles } from "./styles";
 import { INPUT_SIZE } from "./types";
 import { PRIMITIVE_COLORS } from "../../shared";
+import { expandProperty } from "inline-style-expand-shorthand";
 
-const getPlaceholderColor = (isError: boolean, isPositive: boolean, isFocused: boolean): string => {
-  if (isPositive || isFocused) {
+const getInputColor = (isError: boolean, isPositive: boolean, isFocused: boolean): string => {
+  if (isFocused) {
     return PRIMITIVE_COLORS.white;
   }
   if (isError) {
     return PRIMITIVE_COLORS.error400;
+  }
+  if (isPositive) {
+    return PRIMITIVE_COLORS.white;
   }
   return PRIMITIVE_COLORS.primary500;
 };
@@ -30,10 +34,6 @@ export const getInputOverrides = (size: INPUT_SIZE): InputOverrides => {
   return {
     Root: {
       style: ({ $disabled }) => ({
-        borderBottomLeftRadius: "2px",
-        borderBottomRightRadius: "2px",
-        borderTopLeftRadius: "2px",
-        borderTopRightRadius: "2px",
         ...inputContainerModifiedStyles[size],
         ...($disabled ? inputDisabledStyles : {}),
       }),
@@ -46,23 +46,24 @@ export const getInputOverrides = (size: INPUT_SIZE): InputOverrides => {
     Input: {
       style: ({ $error, $positive, $isFocused }) => ({
         ...inputModifiedStyles[size],
+        color: getInputColor($error, $positive, $isFocused),
         "-webkit-text-fill-color": "unset",
 
         "::placeholder": {
-          color: getPlaceholderColor($error, $positive, $isFocused),
+          color: getInputColor($error, $positive, $isFocused),
         },
       }),
     },
     StartEnhancer: {
       style: ({ $error, $positive, $isFocused }) => ({
         color: getIconColor($error, $positive, $isFocused),
-        padding: "0 12px 0 0",
+        ...expandProperty("padding", "0 12px 0 0"),
       }),
     },
     EndEnhancer: {
       style: ({ $error, $positive, $isFocused }) => ({
+        ...expandProperty("padding", "0 0 0 12px"),
         color: getIconColor($error, $positive, $isFocused),
-        padding: "0 0 0 12px",
       }),
     },
     ClearIcon: {
--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -1,15 +1,23 @@
 import { INPUT_SIZE } from "./types";
 import { PRIMITIVE_COLORS } from "../../shared";
+import { BorderRadiusStyles } from "../../shared/styles/border";
+import { expandProperty } from "inline-style-expand-shorthand";
 
+const inputContainerBaseStyles = {
+  ...BorderRadiusStyles,
+};
 export const inputContainerModifiedStyles = {
   [INPUT_SIZE.small]: {
-    padding: "0 12px",
+    ...inputContainerBaseStyles,
+    ...expandProperty("padding", "0 12px"),
   },
   [INPUT_SIZE.medium]: {
-    padding: "0 14px",
+    ...inputContainerBaseStyles,
+    ...expandProperty("padding", "0 14px"),
   },
   [INPUT_SIZE.large]: {
-    padding: "0 16px",
+    ...inputContainerBaseStyles,
+    ...expandProperty("padding", "0 16px"),
   },
 };
 
@@ -17,23 +25,23 @@ export const inputModifiedStyles = {
   [INPUT_SIZE.small]: {
     fontSize: "14px",
     lineHeight: "20px",
-    padding: "8px 0",
+    ...expandProperty("padding", "8px 0"),
   },
   [INPUT_SIZE.medium]: {
     fontSize: "16px",
     lineHeight: "24px",
-    padding: "12px 0",
+    ...expandProperty("padding", "12px 0"),
   },
   [INPUT_SIZE.large]: {
     fontSize: "18px",
     lineHeight: "28px",
-    padding: "14px 0",
+    ...expandProperty("padding", "14px 0"),
   },
 };
 
 export const inputDisabledStyles = {
   backgroundColor: "transparent",
-  borderColor: PRIMITIVE_COLORS.primary600,
+  ...expandProperty("borderColor", PRIMITIVE_COLORS.primary600),
   color: PRIMITIVE_COLORS.primary500,
 };
 
