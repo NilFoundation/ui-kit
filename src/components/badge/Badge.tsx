0ba82ccd3ca060723222118fda34cd06c4f4e48e
Gor Grigoryan <gor.grigoryan@saltpepper.ru>
2023-06-27 17:18:34 +0300

    add badge

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -1,10 +1,17 @@
 import { FC } from "react";
 import { Badge as BaseBadge, BadgeProps as BaseBadgeProps, Hierarchy, SHAPE, COLOR } from "baseui/badge";
+import { getBadgeOverrides } from "./overrides";
+import { BADGE_COLOR, BADGE_HIERARCHY } from "./types";
 
-export type BadgeProps = BaseBadgeProps;
+export type BadgeProps = Omit<BaseBadgeProps, "hierarchy" | "color" | "shape"> & {
+  hierarchy?: BADGE_HIERARCHY;
+  color?: BADGE_COLOR;
+};
 
-const Badge: FC<BadgeProps> = ({ ...props }) => {
-  return <BaseBadge {...props} />;
+const Badge: FC<BadgeProps> = ({ color = BADGE_COLOR.accent, hierarchy = BADGE_HIERARCHY.high, ...props }) => {
+  const overrides = getBadgeOverrides(color, hierarchy);
+
+  return <BaseBadge {...props} overrides={overrides} />;
 };
 
 export { SHAPE, COLOR };
---
+++ src/components/badge/types.ts
@@ -0,0 +1,15 @@
+import { PLACEMENT as BADGE_PLACEMENT } from "baseui/badge";
+
+export enum BADGE_COLOR {
+  accent = "accent",
+  warning = "warning",
+  positive = "positive",
+  negative = "negative",
+}
+
+export enum BADGE_HIERARCHY {
+  low = "low",
+  high = "high",
+}
+
+export { BADGE_PLACEMENT };
