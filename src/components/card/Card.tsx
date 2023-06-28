321464feba047d8fb90dc11de550fb2080bfcb02
Gor Grigoryan <gor.grigoryan@saltpepper.ru>
2023-06-28 13:13:22 +0300

    add card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,24 +2,19 @@ import React, { ReactNode } from "react";
 import { Card as BaseCard, CardProps as BaseCardProps } from "baseui/card";
 import { getCardOverrides } from "./overrides";
 import WhiteDash from "./ui/WhiteDash";
-import { Button } from "../button";
-import { useStyletron } from "baseui";
 
-export type CardProps = BaseCardProps & {
+export type CardProps = Omit<BaseCardProps, "overrides"> & {
   dash?: boolean;
   border?: boolean;
   children: ReactNode;
-  button?: string;
 };
 
-const Card: React.FC<CardProps> = ({ dash = false, border = false, button = false, children, ...props }) => {
+const Card: React.FC<CardProps> = ({ dash = false, border = false, children, ...props }) => {
   const overrides = getCardOverrides(dash, border);
-  const [css] = useStyletron();
   return (
     <BaseCard {...props} overrides={overrides}>
       {dash && <WhiteDash />}
       {children}
-      {button && <Button className={css({ width: "100%" })}>{button}</Button>}
     </BaseCard>
   );
 };
