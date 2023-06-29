053db0a987b6338f498fd7cc912ad89c29b8137c
Gor Grigoryan <gor.grigoryan@saltpepper.ru>
2023-06-29 14:56:59 +0300

    add ability to merge overrides

--- src/components/accordion/Accordion.tsx
+++ src/components/accordion/Accordion.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import { Accordion as BaseAccordion, AccordionProps as BaseAccordionProps } from "baseui/accordion";
 import { getAccordionOverrides } from "./overrides";
+import { getMergedOverrides } from "../../shared/utils/getMergedOverrides";
 
 export type AccordionProps = BaseAccordionProps;
 
-const Accordion: FC<AccordionProps> = ({ ...props }) => {
-  const overrides = getAccordionOverrides();
+const Accordion: FC<AccordionProps> = ({ overrides: baseOverrides, ...props }) => {
+  const accordionOverrides = getAccordionOverrides();
+  const overrides = getMergedOverrides(accordionOverrides, baseOverrides);
 
   return <BaseAccordion {...props} overrides={overrides} />;
 };
