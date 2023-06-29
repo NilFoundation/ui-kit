20b1692fbd0f2a2f00d7a34c8958fe94bb87bd37
Konuhova Alina <alina.konuhova@saltpepper.ru>
2023-06-29 18:02:34 +0300

    add possibilty to override radio

--- src/components/radio/Radio.tsx
+++ src/components/radio/Radio.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
 import { Radio as BaseRadio, RadioProps as BaseRadioProps } from "baseui/radio";
 import { getRadioOverrides } from "./overrides";
+import { getMergedOverrides } from "../../shared/utils/getMergedOverrides";
 
 export type RadioProps = BaseRadioProps;
 
 const Radio: FC<RadioProps> = ({ ...props }) => {
-  const { checked, error, disabled } = props;
+  const { checked, error, disabled, overrides: baseOverrides } = props;
 
-  const overrides = getRadioOverrides(checked, disabled, error);
+  const radioOverrides = getRadioOverrides(checked, disabled, error);
+  const overrides = getMergedOverrides(radioOverrides, baseOverrides);
 
   return <BaseRadio overrides={overrides} {...props} />;
 };
