7ca2a2668970f02d9651e50588f6a6f7e45da6c3
Gor Grigoryan <gor.grigoryan@saltpepper.ru>
2023-06-29 11:16:08 +0300

    fix textarea state behaviour

--- src/components/textarea/Textarea.tsx
+++ src/components/textarea/Textarea.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, FC, useState } from "react";
+import { FC } from "react";
 import { Textarea as BaseTextarea, TextareaProps as BaseTextareaProps } from "baseui/textarea";
 import { getTextareaOverrides } from "./overrides";
 import { TEXTAREA_SIZE } from "./types";
@@ -11,28 +11,15 @@ export type TextareaProps = BaseTextareaProps & {
   isLoading?: boolean;
 };
 
-const Textarea: FC<TextareaProps> = ({
-  value: baseValue,
-  size = TEXTAREA_SIZE.medium,
-  resize,
-  isLoading,
-  onChange,
-  ...props
-}) => {
-  const [value, setValue] = useState<string | number>(baseValue ?? "");
+const Textarea: FC<TextareaProps> = ({ size = TEXTAREA_SIZE.medium, resize, isLoading, ...props }) => {
   const [css] = useStyletron();
 
   const overrides = getTextareaOverrides(size, isLoading);
   const isResizable = resize && ["both", "horizontal", "vertical"].includes(resize);
 
-  const onChangeHandler: ChangeEventHandler<HTMLTextAreaElement> = (event) => {
-    setValue(event.target.value);
-    onChange?.(event);
-  };
-
   return (
     <div className={css({ position: "relative" })}>
-      <BaseTextarea {...props} overrides={overrides} resize={resize} value={value} onChange={onChangeHandler} />
+      <BaseTextarea {...props} overrides={overrides} resize={resize} />
       {isResizable && <TextareaResizeIcon />}
       {isLoading && <TextareaSpinner size={size} />}
     </div>
