e4215bfceae3e03cc8685a0b8adcaa6ae51c467e
Gor Grigoryan <gor.grigoryan@saltpepper.ru>
2023-06-30 13:26:23 +0300

    add tooltip overrides

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { StatefulTooltip, StatefulTooltipProps } from "baseui/tooltip";
 import { getTooltipOverrides } from "./overrides";
+import { getMergedOverrides } from "../../shared/utils/getMergedOverrides";
 
 export type TooltipProps = StatefulTooltipProps;
 
-const Tooltip: React.FC<TooltipProps> = ({ ...props }) => {
-  const overrides = getTooltipOverrides();
+const Tooltip: React.FC<TooltipProps> = ({ overrides: baseOverrides, ...props }) => {
+  const tooltipOverrides = getTooltipOverrides();
+  const overrides = getMergedOverrides(tooltipOverrides, baseOverrides);
 
   return <StatefulTooltip popoverMargin={16} {...props} overrides={overrides} />;
 };
