8fc73d431494ded7c90c77d9e127a315104368ad
Gor Grigoryan <gor.grigoryan@saltpepper.ru>
2023-06-30 13:45:01 +0300

    add tag overrides

--- src/components/tag/Tag.tsx
+++ src/components/tag/Tag.tsx
@@ -2,9 +2,16 @@ import { FC } from "react";
 import { Tag as BaseTag } from "baseui/tag";
 import { TAG_KIND, TAG_VARIANT, TagProps } from "./types";
 import { getTagOverrides } from "./overrides";
+import { getMergedOverrides } from "../../shared/utils/getMergedOverrides";
 
-const Tag: FC<TagProps> = ({ kind = TAG_KIND.primary, variant = TAG_VARIANT.solid, ...props }) => {
-  const overrides = getTagOverrides(kind, variant);
+const Tag: FC<TagProps> = ({
+  kind = TAG_KIND.primary,
+  variant = TAG_VARIANT.solid,
+  overrides: baseOverrides,
+  ...props
+}) => {
+  const tagOverrides = getTagOverrides(kind, variant);
+  const overrides = getMergedOverrides(tagOverrides, baseOverrides);
 
   return <BaseTag {...props} overrides={overrides} />;
 };
