9ef96c65037f5e69f7d7708963b7d6677f6cacc2
Chipi007 <94197615+Chipi007@users.noreply.github.com>
2023-06-30 13:02:35 +0300

    62 custom overrides (#92)
    * add ability to merge overrides
    
    * add possibility to override button
    
    * add possibility to override form control
    
    * add possibility to override input
    
    * add possibility to override menu
    
    * add possibility to override notification
    
    * add possibilty to override radio
    
    * add possibilty to override select
    
    * add possibilty to override textarea
    
    * add file uploader overrides
    
    ---------
    
    Co-authored-by: Gor Grigoryan <gor.grigoryan@saltpepper.ru>
    Co-authored-by: Konuhova Alina <alina.konuhova@saltpepper.ru>

--- src/components/radio/Radio.tsx
+++ src/components/radio/Radio.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
 import { Radio as BaseRadio, RadioProps as BaseRadioProps } from "baseui/radio";
 import { getRadioOverrides } from "./overrides";
+import { getMergedOverrides } from "../../shared/utils/getMergedOverrides";
 
 export type RadioProps = BaseRadioProps;
 
-const Radio: FC<RadioProps> = ({ ...props }) => {
-  const overrides = getRadioOverrides();
+const Radio: FC<RadioProps> = ({ overrides: baseOverrides, ...props }) => {
+  const radioOverrides = getRadioOverrides();
+  const overrides = getMergedOverrides(radioOverrides, baseOverrides);
 
-  return <BaseRadio {...props} overrides={overrides} />;
+  return <BaseRadio overrides={overrides} {...props} />;
 };
 
 export default Radio;
