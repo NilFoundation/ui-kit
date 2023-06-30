6ab0dd282fefbf8b1a27a40792ae8213962970d0
Gor Grigoryan <gor.grigoryan@saltpepper.ru>
2023-06-30 13:14:58 +0300

    add select overrides

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Select as BaseSelect, SelectProps as BaseSelectProps, Value, SIZE } from "baseui/select";
 import { getSelectOverrides } from "./overrides";
 import { SELECT_SIZE } from "./types";
+import { getMergedOverrides } from "../../shared/utils/getMergedOverrides";
 
 export type SelectProps = BaseSelectProps & {
   placeholder: React.ReactNode;
@@ -10,8 +11,16 @@ export type SelectProps = BaseSelectProps & {
   startEnhancer?: React.ReactNode;
 };
 
-const Select: React.FC<SelectProps> = ({ value, disabled, size = SELECT_SIZE.medium, valueKey, ...props }) => {
-  const overrides = getSelectOverrides(size, !!disabled, value, valueKey);
+const Select: React.FC<SelectProps> = ({
+  value,
+  disabled,
+  size = SELECT_SIZE.medium,
+  valueKey,
+  overrides: baseOverrides,
+  ...props
+}) => {
+  const selectOverrides = getSelectOverrides(size, !!disabled, value, valueKey);
+  const overrides = getMergedOverrides(selectOverrides, baseOverrides);
 
   return <BaseSelect {...props} valueKey={valueKey} value={value} disabled={disabled} overrides={overrides} />;
 };
