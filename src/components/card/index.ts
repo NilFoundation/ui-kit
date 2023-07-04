4318a437dde0934ee00bbb336766b58ee31e83ec
Chipi007 <94197615+Chipi007@users.noreply.github.com>
2023-07-04 14:29:31 +0300

    25 add Card (#81)
    * add card #80
    
    * add card
    
    * add card stories
    
    * add card overrides
    
    * rename dash to headline
    
    * rename dash to headline and add description to properties
    
    ---------
    
    Co-authored-by: Konuhova Alina <alina.konuhova@saltpepper.ru>
    Co-authored-by: Gor Grigoryan <gor.grigoryan@saltpepper.ru>
    Co-authored-by: Gor Grigoryan <58621885+gagrigoryan@users.noreply.github.com>

--- src/components/card/index.ts
+++ src/components/card/index.ts
@@ -1,2 +1,13 @@
 export { default as Card } from "./Card";
 export type { CardProps } from "./Card";
+
+export {
+  StyledBody,
+  StyledAction,
+  StyledTitle,
+  StyledRoot,
+  StyledThumbnail,
+  StyledHeaderImage,
+  StyledWrapper,
+  StyledContents,
+} from "baseui/card";
