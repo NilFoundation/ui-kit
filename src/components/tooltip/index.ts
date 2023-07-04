0177b140f93bf0b2789a97a9d313be8312e4abcb
Gor Grigoryan <58621885+gagrigoryan@users.noreply.github.com>
2023-07-04 14:31:29 +0300

    25 add tooltip (#78)
    * add tooltip
    
    * add tooltip overrides
    
    * add tooltip component, fix export
    
    * add tooltip component, fix export
    
    * fix exports
    
    * change tooltip story
    
    ---------
    
    Co-authored-by: Gor Grigoryan <gor.grigoryan@saltpepper.ru>

--- src/components/tooltip/index.ts
+++ src/components/tooltip/index.ts
@@ -1,2 +1,5 @@
+export { default as StatefulTooltip } from "./StatefulTooltip";
+export type { StatefulTooltipProps } from "./StatefulTooltip";
+
 export { default as Tooltip } from "./Tooltip";
 export type { TooltipProps } from "./Tooltip";
