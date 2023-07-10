2f098eafe87fbe7e1e986be41149b1c32d571341
Gor Grigoryan <58621885+gagrigoryan@users.noreply.github.com>
2023-07-10 12:03:28 +0300

    fix CardProps (#102)
    Co-authored-by: Gor Grigoryan <gor.grigoryan@saltpepper.ru>

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,16 +1,9 @@
-import React, { ReactNode } from "react";
-import { Card as BaseCard, CardProps as BaseCardProps } from "baseui/card";
+import React from "react";
+import { Card as BaseCard } from "baseui/card";
 import { getCardOverrides } from "./overrides";
 import WhiteHeadline from "./ui/WhiteHeadline";
 import { getMergedOverrides } from "../../shared/utils/getMergedOverrides";
-
-export type CardProps = BaseCardProps & {
-  /** This property set white headline at the top of the card */
-  headline?: boolean;
-  /** This property set 1px solid grey border to card */
-  border?: boolean;
-  children: ReactNode;
-};
+import { CardProps } from "./types";
 
 const Card: React.FC<CardProps> = ({
   headline = false,
