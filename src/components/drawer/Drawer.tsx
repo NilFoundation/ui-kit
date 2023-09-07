e7e58f493b6f9919de9fd8b6d7d9efbbec421bc3
ukorvl <u.korotovskiy@nil.foundation>
2023-09-08 02:59:07 +0400

    fix Drawer component props #145

--- src/components/drawer/Drawer.tsx
+++ src/components/drawer/Drawer.tsx
@@ -1,24 +1,45 @@
 import { FC } from "react";
-import { ANCHOR, Drawer as BaseDrawer, DrawerProps as BaseDrawerProps } from "baseui/drawer";
+import { ANCHOR, SIZE, Drawer as BaseDrawer, DrawerProps as BaseDrawerProps, DrawerOverrides } from "baseui/drawer";
 import { getDrawerOverrides } from "./overrides";
 import { getMergedOverrides } from "../../shared/utils/getMergedOverrides";
 
-export type DrawerProps = Omit<BaseDrawerProps, "animate" | "closeable"> & {
+type DrawerOwnProps = {
   animate?: boolean;
   closeable?: boolean;
+  overrides?: DrawerOverrides;
+  anchor?: (typeof ANCHOR)[keyof typeof ANCHOR];
+  size?: (typeof SIZE)[keyof typeof SIZE];
+  showBackdrop?: boolean;
+  autoFocus?: boolean;
 };
+
+export type DrawerProps = DrawerOwnProps & Omit<BaseDrawerProps, keyof DrawerOwnProps>;
 
 const Drawer: FC<DrawerProps> = ({
   anchor = ANCHOR.right,
+  size = SIZE.default,
   animate = true,
   closeable = true,
   overrides: baseOverrides,
+  showBackdrop = true,
+  autoFocus = true,
   ...props
 }) => {
   const drawerOverrides = getDrawerOverrides();
   const overrides = getMergedOverrides(drawerOverrides, baseOverrides);
 
-  return <BaseDrawer {...props} anchor={anchor} animate={animate} closeable={closeable} overrides={overrides} />;
+  return (
+    <BaseDrawer
+      {...props}
+      anchor={anchor}
+      size={size}
+      animate={animate}
+      closeable={closeable}
+      showBackdrop={showBackdrop}
+      autoFocus={autoFocus}
+      overrides={overrides}
+    />
+  );
 };
 
 export default Drawer;
