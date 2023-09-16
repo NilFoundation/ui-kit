4f09929db475236c51e5b64e36f48815faf4c8a8
ukorvl <ukorotovskiy@gmail.com>
2023-09-16 18:01:20 +0400

    add highlighting #152

--- src/components/codefield/codeMirrorTheme.ts
+++ src/components/codefield/codeMirrorTheme.ts
@@ -1,15 +1,29 @@
 import { createTheme } from "@uiw/codemirror-themes";
 import { PRIMITIVE_COLORS } from "../../shared";
+import { tags as t } from "@lezer/highlight";
 
 export const codeMirrorTheme = createTheme({
   theme: "dark",
   settings: {
     background: PRIMITIVE_COLORS.gray900,
-    foreground: "#75baff",
-    caret: "#5d00ff",
-    selection: "#036dd626",
-    selectionMatch: "#036dd626",
     lineHighlight: PRIMITIVE_COLORS.gray900,
   },
-  styles: [],
+  styles: [
+    { tag: t.comment, color: PRIMITIVE_COLORS.gray500 },
+    { tag: t.variableName, color: PRIMITIVE_COLORS.blue300 },
+    { tag: [t.string, t.special(t.brace)], color: PRIMITIVE_COLORS.blue300 },
+    { tag: t.number, color: PRIMITIVE_COLORS.green300 },
+    { tag: t.bool, color: PRIMITIVE_COLORS.red300 },
+    { tag: t.null, color: PRIMITIVE_COLORS.gray500 },
+    { tag: t.keyword, color: PRIMITIVE_COLORS.yellow300 },
+    { tag: t.operator, color: PRIMITIVE_COLORS.gray700 },
+    { tag: t.className, color: PRIMITIVE_COLORS.gray500 },
+    { tag: t.definition(t.typeName), color: PRIMITIVE_COLORS.gray500 },
+    { tag: t.typeName, color: PRIMITIVE_COLORS.gray500 },
+    { tag: t.angleBracket, color: PRIMITIVE_COLORS.gray500 },
+    { tag: t.tagName, color: PRIMITIVE_COLORS.gray500 },
+    { tag: t.attributeName, color: PRIMITIVE_COLORS.gray500 },
+    { tag: t.meta, color: PRIMITIVE_COLORS.gray500 },
+    { tag: t.name, color: PRIMITIVE_COLORS.gray500 },
+  ],
 });
