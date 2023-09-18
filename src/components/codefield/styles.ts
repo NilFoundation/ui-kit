939058d7c87856d5218f30b8e9d22cd7863a2cf6
ukorvl <u.korotovskiy@nil.foundation>
2023-09-18 19:57:17 +0400

    change theme #152

--- src/components/codefield/styles.ts
+++ src/components/codefield/styles.ts
@@ -6,12 +6,13 @@ const containerStyles: StyleObject = {
   position: "relative",
   overflow: "hidden",
   background: PRIMITIVE_COLORS.gray900,
-  ...expandProperty("borderRaduis", "4px"),
+  borderRadius: "4px",
   ...expandProperty("padding", "24px"),
   display: "flex",
   justifyContent: "space-between",
   alignItems: "flex-start",
   gap: "12px",
+  color: PRIMITIVE_COLORS.gray100,
 };
 
 export const styles = {
