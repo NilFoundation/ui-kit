3c899145c42d97fe76f3d3b5995f20ded0cdd755
ukorvl <u.korotovskiy@nil.foundation>
2023-09-28 16:19:48 +0400

    remove select duplicate props #159

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { Select as BaseSelect, SelectProps as BaseSelectProps, Value, SIZE } from "baseui/select";
+import { Select as BaseSelect, SelectProps as BaseSelectProps, SIZE } from "baseui/select";
 import { getSelectOverrides } from "./overrides";
 import { SELECT_SIZE } from "./types";
 import { getMergedOverrides } from "../../shared/utils/getMergedOverrides";
 
 export type SelectProps = Omit<BaseSelectProps, "size"> & {
-  placeholder: React.ReactNode;
-  onChange?: (value: Value) => void;
   size?: SELECT_SIZE;
-  startEnhancer?: React.ReactNode;
 };
 
 const Select: React.FC<SelectProps> = ({
