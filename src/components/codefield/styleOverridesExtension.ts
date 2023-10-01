3b024cf1957c42a2463dcc2bd0f9a4efbfce2c4c
ukorvl <u.korotovskiy@nil.foundation>
2023-10-01 16:47:16 +0400

    minor changes #152

--- src/components/codefield/styleOverridesExtension.ts
+++ src/components/codefield/styleOverridesExtension.ts
@@ -1,6 +1,6 @@
 import { EditorView } from "@codemirror/view";
 
-export const styleOverridesExtension = EditorView.theme({
+export const styleOverridesExtension = EditorView.baseTheme({
   "&": {
     "&.cm-focused": {
       outline: "none",
