3f84f95125616b667bcce8fbcefb61a79d3db5f9
Uriy Korotovskikh <u.korotovskiy@nil.foundation>
2023-10-09 16:55:20 +0400

    add CodeFiled copy button tooltip #166

--- src/components/codefield/useCopyToClipboard.ts
+++ src/components/codefield/useCopyToClipboard.ts
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { CodeFieldProps } from "./CodeField";
 import copy from "copy-to-clipboard";
 
@@ -6,14 +7,16 @@ export const useCopyToClipboard = (
   onCopy?: CodeFieldProps["onCopy"],
   transformOnCopy?: CodeFieldProps["transformOnCopy"]
 ) => {
-  const onClick = () => {
+  const onClick = useCallback(() => {
     const codeToCopy = transformOnCopy ? transformOnCopy(code) : code;
     const result = copy(codeToCopy);
 
     if (onCopy) {
       onCopy(codeToCopy, result);
     }
-  };
+
+    return result;
+  }, [code, onCopy, transformOnCopy]);
 
   return onClick;
 };
