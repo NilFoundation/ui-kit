22e9e23869df4c45e01ac34724401f1ffe21860e
ukorvl <u.korotovskiy@nil.foundation>
2023-10-18 12:31:21 +0400

    minor changes #169

--- src/components/chart/index.ts
+++ src/components/chart/index.ts
@@ -1,6 +1,7 @@
 export { default as Chart } from "./Chart";
 export type { ChartProps } from "./Chart";
 export { default as ChartToolbar } from "./toolbar/ChartToolbar";
+export { default as ChartLegend } from "./ChartLegend";
 export * from "./series";
 export * from "./scales";
 export * from "./toolbar";
