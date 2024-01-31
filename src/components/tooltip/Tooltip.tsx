8dcaeb97264b75c6da3f20972459c436f1ea9c46
ukorvl <u.korotovskiy@nil.foundation>
2024-01-31 13:13:35 +0400

    fix tooltip stories #246

--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { Tooltip as BaseTooltip, TooltipProps as BaseTooltipProps } from "baseui/tooltip";
+import { Tooltip as BaseTooltip } from "baseui/tooltip";
 import { getTooltipOverrides } from "./overrides";
 import { getMergedOverrides } from "../../shared/utils/getMergedOverrides";
-import { TOOLTIP_KIND, TooltipCommonProps } from "./types";
-
-export type TooltipProps = BaseTooltipProps & TooltipCommonProps;
+import { TOOLTIP_KIND, TooltipProps } from "./types";
 
 const Tooltip: React.FC<TooltipProps> = ({ overrides: baseOverrides, kind = TOOLTIP_KIND.DEFAULT, ...props }) => {
   const tooltipOverrides = getTooltipOverrides(kind);
   const overrides = getMergedOverrides(tooltipOverrides, baseOverrides);
 
-  return <BaseTooltip popoverMargin={16} {...props} overrides={overrides} />;
+  return <BaseTooltip popoverMargin={4} {...props} overrides={overrides} />;
 };
 
 export default Tooltip;
--- src/components/tooltip/types.ts
+++ src/components/tooltip/types.ts
@@ -1,3 +1,5 @@
+import { TooltipProps as BaseTooltipProps, StatefulTooltipProps as BaseStatefulTooltipProps } from "baseui/tooltip";
+
 export enum TOOLTIP_KIND {
   DEFAULT = "default",
   ERROR = "error",
@@ -7,3 +9,7 @@ export enum TOOLTIP_KIND {
 export type TooltipCommonProps = {
   kind?: TOOLTIP_KIND;
 };
+
+export type TooltipProps = BaseTooltipProps & TooltipCommonProps;
+
+export type StatefulTooltipProps = BaseStatefulTooltipProps & TooltipCommonProps;
