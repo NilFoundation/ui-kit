c426556bb28caf3d7b69350f2a8fb9464f1aea7d
ukorvl <ukorotovskiy@gmail.com>
2024-03-28 02:15:02 +0400

    minor changes

--- src/components/copy-button/CopyButton.test.tsx
+++ src/components/copy-button/CopyButton.test.tsx
@@ -1,8 +1,7 @@
-import { screen, waitFor } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import CopyButton from "./CopyButton";
 import { render } from "../../test-utils/render";
 import { createComponentSSRTest } from "../../test-utils/createComponentSSRTest";
-import userEvent from "@testing-library/user-event";
 
 describe("CopyButton", () => {
   it("renders without crashing", () => {
@@ -10,17 +9,6 @@ describe("CopyButton", () => {
 
     const el = screen.getByTestId("Button");
     expect(el).toBeInTheDocument();
-  });
-
-  it("handles onCopy", async () => {
-    const onCopy = jest.fn();
-    render(<CopyButton textToCopy="Text" onCopy={onCopy} data-testid="Button" />);
-
-    const el = screen.getByTestId("Button");
-    userEvent.click(el);
-    await waitFor(() => expect(onCopy).toHaveBeenCalledTimes(1));
-
-    //jest.clearAllMocks();
   });
 
   it("renders ssr without crashing", () => {
