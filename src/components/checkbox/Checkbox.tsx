fb301d5616457d3d5e93a84a718a593faaffe110
ukorvl <ukorotovskiy@gmail.com>
2024-04-10 19:56:45 +0400

    add tap area to checkbox radio and toggle #242

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -1,18 +1,32 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 import { Checkbox as BaseCheckbox, LABEL_PLACEMENT } from "baseui/checkbox";
 import { getCheckboxOverrides } from "./overrides";
 import { getMergedOverrides } from "../../shared/utils/getMergedOverrides";
 import { CheckboxProps } from "./types";
+import { TapArea } from "../../shared/ui/tap-area";
 
 const Checkbox: FC<CheckboxProps> = ({
   labelPlacement = LABEL_PLACEMENT.right,
   overrides: baseOverrides,
+  inputRef,
   ...props
 }) => {
   const checkboxOverrides = getCheckboxOverrides();
   const overrides = getMergedOverrides(checkboxOverrides, baseOverrides);
+  const checkboxRef = useRef<HTMLInputElement>(null);
+  const finalRef = inputRef || checkboxRef;
 
-  return <BaseCheckbox {...props} overrides={overrides} labelPlacement={labelPlacement} checkmarkType="default" />;
+  return (
+    <TapArea onClick={() => finalRef.current?.click()}>
+      <BaseCheckbox
+        {...props}
+        overrides={overrides}
+        labelPlacement={labelPlacement}
+        checkmarkType="default"
+        inputRef={finalRef}
+      />
+    </TapArea>
+  );
 };
 
 export { LABEL_PLACEMENT };
