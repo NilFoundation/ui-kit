3e50b7dc18c7b6e6def787feea2e916adc11a541
ukorvl <ukorotovskiy@gmail.com>
2024-04-18 01:00:05 +0400

    add infinite prgress bar #112

--- src/components/progress-bar/types.ts
+++ src/components/progress-bar/types.ts
@@ -1,5 +1,18 @@
+import { ProgressBarProps as BaseProgressBarProps } from "baseui/progress-bar";
+
 export enum PROGRESS_BAR_SIZE {
   small = "small",
   medium = "medium",
   large = "large",
 }
+
+export type ProgressBarProps = Omit<
+  BaseProgressBarProps,
+  "steps" | "size" | "showLabel" | "successValue" | "getProgressLabel" | "infinite"
+> & {
+  size?: PROGRESS_BAR_SIZE;
+  className?: string;
+  showLabel?: boolean;
+  getProgressLabel?: (value: number, minValue: number, maxValue: number) => string;
+  infinite?: boolean;
+};
