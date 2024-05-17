e7f914efe81ef92e0fcd8c4d3007ce08342f86c1
ukorvl <ukorotovskiy@gmail.com>
2024-05-17 23:30:39 +0300

    minor cahnges #265

--- src/components/chart/series/plugins/CustomHistogramSeries/renderer.ts
+++ src/components/chart/series/plugins/CustomHistogramSeries/renderer.ts
@@ -1,75 +1,48 @@
 import { BitmapCoordinatesRenderingScope, CanvasRenderingTarget2D } from "fancy-canvas";
-import {
-  ICustomSeriesPaneRenderer,
-  PaneRendererCustomData,
-  PriceToCoordinateConverter,
-  Time,
-} from "lightweight-charts";
+import { ICustomSeriesPaneRenderer, PaneRendererCustomData, Time } from "lightweight-charts";
 import { CustomHistogramSeriesData, CustomHistogramSeriesOptions } from "./types";
 
 export class CustomHistogramRenderer implements ICustomSeriesPaneRenderer {
-  _data: PaneRendererCustomData<Time, TData> | null = null;
+  _data: PaneRendererCustomData<Time, CustomHistogramSeriesData> | null = null;
   _options: CustomHistogramSeriesOptions | null = null;
 
-  draw(target: CanvasRenderingTarget2D, priceConverter: PriceToCoordinateConverter): void {
-    target.useBitmapCoordinateSpace((scope) => this._drawImpl(scope, priceConverter));
+  draw(target: CanvasRenderingTarget2D): void {
+    target.useBitmapCoordinateSpace((scope) => this._drawImpl(scope));
   }
 
-  update(data: PaneRendererCustomData<Time, TData>, options: CustomHistogramSeriesOptions): void {
+  update(data: PaneRendererCustomData<Time, CustomHistogramSeriesData>, options: CustomHistogramSeriesOptions): void {
     this._data = data;
     this._options = options;
   }
 
-  _drawImpl(renderingScope: BitmapCoordinatesRenderingScope, priceToCoordinate: PriceToCoordinateConverter): void {
-    if (
-      this._data === null ||
-      this._data.bars.length === 0 ||
-      this._data.visibleRange === null ||
-      this._options === null
-    ) {
+  _drawImpl({ context: ctx, verticalPixelRatio }: BitmapCoordinatesRenderingScope): void {
+    if (this._data === null || this._data.bars.length === 0 || this._data.visibleRange === null) {
       return;
     }
-    const options = this._options;
-    const bars: _CLASSNAME_Item[] = this._data.bars.map((bar) => {
-      return {
-        x: bar.x * renderingScope.horizontalPixelRatio,
-        high: priceToCoordinate(bar.originalData.high)! * renderingScope.verticalPixelRatio,
-        low: priceToCoordinate(bar.originalData.low)! * renderingScope.verticalPixelRatio,
-      };
-    });
 
-    const ctx = renderingScope.context;
-    ctx.beginPath();
-    const lowLine = new Path2D();
-    const highLine = new Path2D();
-    const firstBar = bars[this._data.visibleRange.from];
-    lowLine.moveTo(firstBar.x, firstBar.low);
-    for (let i = this._data.visibleRange.from + 1; i < this._data.visibleRange.to; i++) {
-      const bar = bars[i];
-      lowLine.lineTo(bar.x, bar.low);
+    const tickWidth = Math.max(1, Math.floor(verticalPixelRatio));
+    const histogramBase = Math.round(this._data.histogramBase * verticalPixelRatio);
+    const topHistogramBase = histogramBase - Math.floor(tickWidth / 2);
+    const bottomHistogramBase = topHistogramBase + tickWidth;
+
+    for (let i = this._data.visibleRange.from; i < this._data.visibleRange.to; i++) {
+      const item = this._data.items[i];
+      const current = this._precalculatedCache[i - this._data.visibleRange.from];
+      const y = Math.round(item.y * verticalPixelRatio);
+      ctx.fillStyle = item.barColor;
+
+      let top: number;
+      let bottom: number;
+
+      if (y <= topHistogramBase) {
+        top = y;
+        bottom = bottomHistogramBase;
+      } else {
+        top = topHistogramBase;
+        bottom = y - Math.floor(tickWidth / 2) + tickWidth;
+      }
+
+      ctx.fillRect(current.left, top, current.right - current.left + 1, bottom - top);
     }
-
-    const lastBar = bars[this._data.visibleRange.to - 1];
-    highLine.moveTo(lastBar.x, lastBar.high);
-    for (let i = this._data.visibleRange.to - 2; i >= this._data.visibleRange.from; i--) {
-      const bar = bars[i];
-      highLine.lineTo(bar.x, bar.high);
-    }
-
-    const area = new Path2D(lowLine);
-    area.lineTo(lastBar.x, lastBar.high);
-    area.addPath(highLine);
-    area.lineTo(firstBar.x, firstBar.low);
-    area.closePath();
-    ctx.fillStyle = options.areaColor;
-    ctx.fill(area);
-
-    ctx.lineJoin = "round";
-    ctx.strokeStyle = options.lowLineColor;
-    ctx.lineWidth = options.lowLineWidth * renderingScope.verticalPixelRatio;
-    ctx.stroke(lowLine);
-    ctx.strokeStyle = options.highLineColor;
-    ctx.lineWidth = options.highLineWidth * renderingScope.verticalPixelRatio;
-    ctx.stroke(highLine);
   }
 }
