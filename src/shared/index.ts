cd8c79105acac3eb5a2effdc540c94f4036e7382
ukorvl <u.korotovskiy@nil.foundation>
2024-11-27 17:19:05 +0400

    export encodeInlineSvg()

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,3 +1,4 @@
 export { createTheme, COLORS, PRIMITIVE_COLORS, svgInlineDotsPattern, SPACE } from "./theme";
 export type { CreateTheme, CreateThemeOptions, CreateThemeReturnType, DefaultFonts } from "./theme";
 export type { LinkComponentRenderFunction, LinkComponentProps } from "./ui/getCustomLinkComponent";
+export { encodeInlineSvg } from "./utils/encodeInlineSvg";
